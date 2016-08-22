70db2cb775f6d1a6868a09daa69dd21e5a89c0a4
Pjotor87 <andersson.peter0@gmail.com>
2016-08-22 08:46:03 +0200

    Commit and push from bash script.

--- uploadchangestogit.bash
+++ uploadchangestogit.bash
@@ -2,6 +2,7 @@ git add uploadchangestogit.bash
 git add vimrc
 git add installscript.bash
 git add VimTODO.txt
+git add VimHotKeys.txt
 
 git commit -m "Commit and push from bash script."
 git push -u origin master
